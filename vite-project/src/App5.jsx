9c9191303675643236bdf84cbdf912cd15a33729
maybeags <suren0709@gmail.com>
2025-02-27 19:21:18 +0900

    feat: button review

--- vite-project/src/App5.jsx
+++ vite-project/src/App5.jsx
@@ -166,3 +166,21 @@
 // 내일 그러면 <p> 태그와 <button>태그로 이루어진
 // 완벽한 형태의 Counter 컴포넌트를 작성하여 실행하는 것부터
 // 수업을 시작할 예정
+
+import { useState } from "react";
+import './App.css'
+
+function Counter() {
+  const [ count, setCount ] = useState(0);
+
+  return(
+  <>
+    <p>Current Count Number : {count}</p> 
+    <button onClick={() => setCount(Precount => Precount + 1)}>
+          증가
+        </button>
+  </>
+  );
+}
+/*Component 상태 조회는 중괄호{}*/
+export default Counter;
