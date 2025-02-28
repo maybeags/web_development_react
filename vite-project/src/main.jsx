03b4664ce6de32b885d07703700c42eded722101
maybeags <suren0709@gmail.com>
2025-02-28 20:21:34 +0900

    feat: flowChart uploaded

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -10,8 +10,10 @@ import ReactDOM from 'react-dom/client'
 // import Counter from './App5.jsx'
 // import App6 from './App6.jsx'
 // import Counter from './App7.jsx'
-import App8 from './App8.jsx'
-import Counter from './App9.jsx'
+// import App8 from './App8.jsx'
+// import Counter from './App9.jsx'
+// import App from './App10.jsx'
+import App from './App11.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -27,6 +29,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <App6 /> */}
     {/* <Counter /> */}
     {/* <App8 /> */}
-    <Counter />
+    {/* <Counter /> */}
+    <App />
   </React.StrictMode>,
 )
