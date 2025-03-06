4ea4ff31d7f3508fe829e161be67c10fe9e2f348
maybeags <suren0709@gmail.com>
2025-03-06 19:31:32 +0900

    feat: api address uploaded

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -1,7 +1,13 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [weather, setWeather] = useState({
+    temp: '',
+    desc: '',
+    icon: '',
+  });
+
   return (
     <>
       
