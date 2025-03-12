16ef947efa1c81fd10133c61cbd2249c6eda7820
maybeags <suren0709@gmail.com>
2025-03-12 20:03:14 +0900

    feat: 20250312 lesson

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,26 +1,28 @@
 // import Header from "./components/Header";
+// import 이미지변수명 from "경로";
+import reactImg from './assets/react-core-concepts.png';
 
 const reactDescriptionsKor = [ '근본적인', '결정적인', '핵심적인' ];
 const reactDescriptionsEng = [ 'Fundamental', 'Crucial', 'Core' ];
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1)); // 얘의 return값은 0 ~ 2의 int가 반환됨
+  return Math.floor(Math.random() * (max)); // 얘의 return값은 0 ~ 2의 int가 반환됨
   // 어떻게? Math.random() * (max + 1) 얘의 범위가 0.xxxx ~ 2.xxxx이기 때문
 }
 
 function Header() {
-
+  const indexNum = getRandomInt(reactDescriptionsEng.length);
 
   return(
     <header>
-    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <img src={reactImg} alt="Stylized atom" />
     <h1>React 필수 요소 복습</h1>
     <p>
-      {reactDescriptionsEng[getRandomInt(2)]} React concepts you will need for almost any app you are
+      {reactDescriptionsEng[indexNum]} React concepts you will need for almost any app you are
       going to build!
 
       <br />
-      대부분의 앱을 위해 당신이 필요로 할 {reactDescriptionsKor[getRandomInt(2)]} React 개념 학습 😀
+      대부분의 앱을 위해 당신이 필요로 할 {reactDescriptionsKor[indexNum]} React 개념 학습 😀
     </p>
   </header>
   );
