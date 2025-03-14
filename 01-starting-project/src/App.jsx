cf7da59f107dca4ba99c330e27f3d396f198bbff
maybeags <suren0709@gmail.com>
2025-03-14 20:17:31 +0900

    feat : 20250314 task # 9

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -2,14 +2,17 @@ import { CORE_CONCEPTS } from "./data";
 import Header2 from "./components/Header/Header2";
 import CoreConcepts from "./components/CoreConcepts/CoreConcepts";
 import TabButton from "./components/TabButton";
+import { useState } from "react";
+import { EXAMPLES } from "./data_updated";
 
 function App() {
 
-  // const [ selectedTopic, setSelectedTopic ] = useState('');
+  const [ selectedTopic, setSelectedTopic ] = useState('');
 
   function handleSelect(selectedTopic) {
     // selectedButton = 'components', 'jsx', 'props', 'state'
     console.log(selectedTopic);
+    setSelectedTopic(selectedTopic);
   }
 
   return (
@@ -31,12 +34,20 @@ function App() {
         <section id="examples">
           <h2>예시들</h2>
           <menu>
-            <TabButton onSelect={handleSelect('굿나잇')}>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          동적인 버튼 클릭의 결과물
+          <div id="tab-content">
+            <h3>{selectedTopic}</h3>
+            <p>description</p>
+            <pre>
+              <code>
+                code
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
