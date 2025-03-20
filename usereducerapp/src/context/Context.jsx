4cc43dfb815535a0c7c8d719f3107a1308a5f725
maybeags <suren0709@gmail.com>
2025-03-20 19:20:39 +0900

    feat: Context.jsx 작성중

--- usereducerapp/src/context/Context.jsx
+++ usereducerapp/src/context/Context.jsx
@@ -1,3 +1,12 @@
 import { createContext, useReducer } from "react";
 import Reducer from "../reducer/Reducer";
 
+const INITIAL_STATE = { isLogin: false, message: '' };
+
+export const Context = createContext(INITIAL_STATE);
+
+export const ContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
+}
+
+export default Context;
