c15a680226fa4c390f63457867e1157c5ce25329
maybeags <suren0709@gmail.com>
2025-03-20 19:57:49 +0900

    fix :  error fixed

--- usereducerapp/src/context/Context.jsx
+++ usereducerapp/src/context/Context.jsx
@@ -3,10 +3,21 @@ import Reducer from "../reducer/Reducer";
 
 const INITIAL_STATE = { isLogin: false, message: '' };
 
-export const Context = createContext(INITIAL_STATE);
+export const Context = createContext();
 
 export const ContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
+
+  return(
+    <Context.Provider
+      value= {{
+        state,
+        dispatch,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
 }
 
 export default Context;
