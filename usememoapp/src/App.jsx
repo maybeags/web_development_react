1dd4428d681ac4752f5c15ef4d62d97e15964604
maybeags <suren0709@gmail.com>
2025-03-21 19:09:43 +0900

    feat: 20250321 lesson

--- usememoapp/src/App.jsx
+++ usememoapp/src/App.jsx
@@ -1,4 +1,15 @@
+import { useMemo, useState, useEffect } from "react";
+
 function App() {
+  const [ number, setNumber ] = useState(0);
+  const [ isTrue, setIsTrue ] = useState(true);
+
+  const answer = isTrue ? "true" : "false";
+
+  // answer가 바뀔 때만 콘솔이 찍히도록 useEffect 적용
+  useEffect(() => {
+    console.log('answer 값이 변경되었습니다.');
+  }, [answer]);
 
   return(
     <>
