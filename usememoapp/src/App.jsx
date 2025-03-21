8d2eb08a0041360c884e45a7bc8b294cef73245b
maybeags <suren0709@gmail.com>
2025-03-21 19:30:25 +0900

    feat: 20250321 오늘 말이 빨라서 죄송합니다...

--- usememoapp/src/App.jsx
+++ usememoapp/src/App.jsx
@@ -1,10 +1,13 @@
 import { useMemo, useState, useEffect } from "react";
+import './App.css';
 
 function App() {
   const [ number, setNumber ] = useState(0);
   const [ isTrue, setIsTrue ] = useState(true);
 
-  const answer = isTrue ? "true" : "false";
+  // const answer = isTrue ? "true" : "false"; 기초자료형(primitive type)
+
+  const answer = { bool: isTrue ? "true" : "false" };
 
   // answer가 바뀔 때만 콘솔이 찍히도록 useEffect 적용
   useEffect(() => {
@@ -12,9 +15,20 @@ function App() {
   }, [answer]);
 
   return(
-    <>
-    
-    </>
+    <div>
+      <p>✨ number</p>
+      <input 
+        type="number" 
+        value={number}
+        onChange ={(e) => setNumber(e.target.value)}
+      />
+      <hr />
+      <p>✨ answer</p>
+      {/* <p> True or False? : {answer} </p> */}
+      {/* <p> True or False? : {answer.bool} </p> */}
+      <p> True or False? : {answer['bool']} </p>
+      <button onClick={() => setIsTrue(!isTrue)}>Reverse</button>
+    </div>
   );
 }
 
