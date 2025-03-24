671f897279bab91e6823e6f1f2056cd2376c5b90
maybeags <suren0709@gmail.com>
2025-03-24 20:10:52 +0900

    feat: movie app started

--- coinapriceapp/src/App.jsx
+++ coinapriceapp/src/App.jsx
@@ -1,16 +1,24 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [ loading, setLoading ] = useState(true);
+  const [ coins, setCoins ] = useState([]);
+
+  // setLoading(false);를 적용하게 되는 시점이 언제인가? 
+  // 1.
+
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+    .then(response => response.json())  // 2.
+    .then(json => console.log(json));
+  }, []);
+
 
   return (
     <div>
       <h1>Coin Price Tracker</h1>
-      <ul>
-        <li>
-          <h5>코인 이름1</h5>
-          <p> 현재 가격1</p>
-        </li>
-      </ul>
+      {loading ? <strong>Loading...🛴</strong> : null}
     </div>
   )
 }
