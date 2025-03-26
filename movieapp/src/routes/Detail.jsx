743a24a57b305e01fa9ce63b545d6e4178cc9b6a
maybeags <suren0709@gmail.com>
2025-03-26 19:24:45 +0900

    feat: detail api uploaded

--- movieapp/src/routes/Detail.jsx
+++ movieapp/src/routes/Detail.jsx
@@ -1,6 +1,24 @@
-
+import { useState, useEffect } from "react";
 
 function Detail() {
+  const [ loading, setLoading ] = useState(true);
+  const [ movie, setMovie ] = useState(null);
+
+  const getMovie = async () => {
+    try {
+      const response = await fetch(
+        ``
+      )
+    } catch (error) {
+
+    }
+  }
+
+  useEffect(() => {
+    //여기에 getMovie();
+  }, []); 
+
+
   return <h1>Detail Page</h1>
 }
 
